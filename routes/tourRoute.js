107fb58cd3cd4d8da0d478a140b7e7feb54dcb78
ishraque-dev <ahamedesrak3@gmail.com>
2023-02-07 23:58:26 +0600

    api features implemented

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -5,10 +5,12 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  aliasTopTour,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 // router.param('id', checkId);
+router.route('/top-5-cheap').get(aliasTopTour, getAllTour);
 router.route('/').get(getAllTour).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 module.exports = router;
---
+++ utils/apiFeatures.js
@@ -0,0 +1,58 @@
+class APIfeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    // removing the unwanted query
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((field) => delete queryObj[field]);
+    // ADVANCED FILTERING
+    // converting a Obj into string and editing the property
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      // eslint-disable-next-line no-return-assign
+      (match) => (match = `$${match}`)
+    );
+
+    this.query = this.query.find(JSON.parse(queryString));
+    return this;
+  }
+
+  sort() {
+    // SORTING
+    if (this.queryString.sort) {
+      // Sorted by req.query.sort
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      // Default sorting
+      this.query = this.query.sort('createdAt');
+    }
+    return this;
+  }
+
+  limit() {
+    // FIELD LIMITING
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+
+  pagination() {
+    // PAGINATION
+    const page = +this.queryString.page || 1;
+    const limit = +this.queryString.limit || 10;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+module.exports = APIfeatures;
