26569f976f0b4511df250912f4e617c0ac7f4b70
ishraque-dev <ahamedesrak3@gmail.com>
2023-02-20 21:34:05 +0600

    working on error DB handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,42 @@
-module.exports = (error, req, res, next) => {
-  error.statusCode = error.statusCode || 500;
-  error.status = error.status || 'error';
+const AppError = require('../utils/appError');
+
+const handleValidationErrorDB = function (err) {
+  const message = `${err.message}`;
+  return new AppError(message, 400);
+};
+const sedErrorDev = function (error, res) {
   res.status(error.statusCode).json({
     status: error.status,
     message: error.message,
+    stack: error.stack,
+    error: error.errors,
   });
+};
+const sendErrorProduction = function (error, res) {
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    // console.error('Error:', error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
+module.exports = (error, req, res, next) => {
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || 'error';
+  if (process.env.NODE_ENV === 'development') {
+    sedErrorDev(error, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let handledError;
+    if (error.name === 'ValidationError') {
+      handledError = handleValidationErrorDB(error);
+    }
+    sendErrorProduction(handledError, res);
+  }
 };
