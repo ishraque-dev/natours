76e5578290e4cdd725adb4490181cedb216c0bd0
ishraque-dev <ahamedesrak3@gmail.com>
2023-02-22 20:31:34 +0600

    jwt implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,9 +8,12 @@ exports.signup = catchAsync(async function (req, res, next) {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
-  const token = jwt.sign({ id: newUser._id }, '');
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
