5a3965f04fedbcf28ebfafd1dba6a2168c9dc74c
ishraque-dev <ahamedesrak3@gmail.com>
2023-02-26 20:00:44 +0600

    Just a comment added for better understanding

--- models/userModel.js
+++ models/userModel.js
@@ -55,6 +55,7 @@ const UserSchema = new mongoose.Schema({
   },
 });
 UserSchema.pre('save', async function (next) {
+  // Only manipulate the password if the password field has changed.
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
   this.passwordConfirm = undefined;
